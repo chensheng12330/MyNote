3c9442dec4ce4051cbb40b2453a9580c0973da7a
1322 <admin@router.router>
2012-11-16 23:12:20 +0800

    add 2 file

---
+++ MyNote/MyData.h
@@ -0,0 +1,13 @@
+//
+//  MyData.h
+//  MyNote
+//
+//  Created by 1322 on 12-11-16.
+//  Copyright (c) 2012年 1322. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyData : NSObject
+
+@end
---
+++ MyNote/MyData.m
@@ -0,0 +1,13 @@
+//
+//  MyData.m
+//  MyNote
+//
+//  Created by 1322 on 12-11-16.
+//  Copyright (c) 2012年 1322. All rights reserved.
+//
+
+#import "MyData.h"
+
+@implementation MyData
+
+@end
